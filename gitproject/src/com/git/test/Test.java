5fc74d43b1fc19876c8637254fc2bafb379dc02b
Siddharth <Siddharth@FATPIPE-PC>
2017-10-16 20:31:03 -0600

    commited for first time

---
+++ gitproject/src/com/git/test/Test.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World kiran first devops run 1st commit");
+
+	}
+
+}
